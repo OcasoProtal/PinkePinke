e2832b3e78f7546c24c79ed179c25fb9fafc6c7d
Ocaso Protal <ocasoprotal@gmail.com>
2024-07-26 14:51:22 +0200

    set inputs

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,81 @@
 <template>
-  <q-page class="flex flex-center"> Main </q-page>
+  <q-page class="text-center bg-secondary">
+    <div class="row">
+      <div class="col">
+        <!-- Date -->
+        <q-input
+          outlined
+          v-model="date"
+          label="TRADate"
+          mask="date"
+          :rules="['date']"
+        >
+          <template v-slot:append>
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy
+                cover
+                transition-show="scale"
+                transition-hide="scale"
+              >
+                <q-date v-model="date">
+                  <div class="row items-center justify-end">
+                    <q-btn v-close-popup label="Close" color="primary" flat />
+                  </div>
+                </q-date>
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <!-- Description -->
+        <q-input outlined label="TRADescription" v-model="description" />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <!-- Participants -->
+        <q-select
+          v-model="participants"
+          :options="allPersons"
+          outlined
+          multiple
+          label="TRAParticipants"
+          class="q-pt-lg"
+        />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <q-input
+          outlined
+          label="TRAAmount"
+          v-model.number="amount"
+          class="q-mt-lg"
+        />
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script setup>
+import { ref } from "vue";
 defineOptions({
   name: "IndexPage",
 });
+
+/*
+Data setup
+*/
+const date = ref("");
+const description = ref("");
+const participants = ref(null);
+const allPersons = ref([
+  { label: "Horst", value: "1" },
+  { label: "Schorsch", value: "2" },
+  { label: "Dieter", value: "3" },
+]);
+const amount = ref(0.0);
 </script>
