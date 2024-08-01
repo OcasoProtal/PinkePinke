38275131f379ce1488a17172b6cc0c56d03b8568
Ocaso Protal <ocasoprotal@gmail.com>
2024-08-01 18:03:29 +0200

    more meat for settings

---
+++ src/components/ParticipantsList.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="text-h6">TRAParticipantsList</div>
+  <!-- <q-table
+    title="TRAParticipants"
+    class="bg-secondary"
+    :rows="activityStore.activities"
+    :columns="columns"
+    row-key="name"
+  /> -->
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+/*
+Main setup
+*/
+
+/*
+Data Setup
+*/
+// const columns = [
+//   {
+//     name: "date",
+//     required: true,
+//     label: "TRADate",
+//     align: "left",
+//     field: "date",
+//     format: (val) => `${val}`,
+//     sortable: true,
+//   },
+//   {
+//     name: "description",
+//     label: "TRADescription",
+//     align: "left",
+//     field: "description",
+//     sortable: true,
+//   },
+//   {
+//     name: "participants",
+//     label: "TRAParticipants",
+//     align: "left",
+//     field: "participants",
+//     format: (val) =>
+//       `${val
+//         .map((v) => v.label)
+//         .sort()
+//         .join(", ")}`,
+//     sortable: true,
+//   },
+//   {
+//     name: "amount",
+//     label: "TRAAmount",
+//     field: "amount",
+//     sortable: true,
+//     sort: (a, b) => parseFloat(a, 10) - parseFloat(b, 10),
+//   },
+// ];
+</script>
--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -1,12 +1,49 @@
 <template>
   <q-page class="text-center bg-secondary">
     <div class="text-h4 q-py-md">TRASettings</div>
-    <div class="text-h6 q-py-md q-pl-md float-left">TRAParticipants</div>
+
+    <div class="row">
+      <div class="text-h6 q-py-md float-left">TRAParticipants</div>
+    </div>
+    <div class="row">
+      <div class="col-5">
+        <q-input outlined label="TRAName" v-model="name" />
+      </div>
+      <div class="col-5">
+        <q-input outlined label="TRAMail" type="email" v-model="mail" />
+      </div>
+      <div class="col">
+        <q-btn round color="primary" icon="add" @click="addParticipant" />
+      </div>
+    </div>
+    <div class="row">
+      <ParticipantsList />
+    </div>
   </q-page>
 </template>
 
 <script setup>
-defineOptions({
-  name: "SettingsPage",
-});
+/*
+Imports
+*/
+import { ref } from "vue";
+import ParticipantsList from "src/components/ParticipantsList.vue";
+
+/*
+Main setup
+*/
+
+const name = ref("");
+const mail = ref("");
+
+/*
+Data setup
+*/
+
+/*
+Actions
+*/
+function addParticipant() {
+  console.log(name.value, mail.value);
+}
 </script>
