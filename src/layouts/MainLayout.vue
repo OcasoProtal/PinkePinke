5136e1f27ba2236e486ebd7b00d98c47706ab852
Ocaso Protal <ocasoprotal@gmail.com>
2024-08-02 15:11:58 +0200

    changed some colors

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -16,7 +16,13 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered elevated>
+    <q-drawer
+      class="bg-secondary"
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      elevated
+    >
       <q-list>
         <q-item-label header> Übersicht </q-item-label>
         <q-item
