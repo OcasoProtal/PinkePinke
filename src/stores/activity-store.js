f720edf9107ebeb4b47496081a867d5e7c0860d0
Ocaso Protal <ocasoprotal@gmail.com>
2024-08-06 11:44:22 +0200

    added comments

--- src/stores/activity-store.js
+++ src/stores/activity-store.js
@@ -3,10 +3,15 @@ import { defineStore } from "pinia";
 import { LocalStorage } from "quasar";
 
 export const useActivityStore = defineStore("activity", () => {
+  // State
   const localStoreActivities =
     JSON.parse(LocalStorage.getItem("Activities")) || [];
   const activities = ref(localStoreActivities);
+
+  // Getters
   const count = computed(() => activities.value.length);
+
+  // Actions
   function addActivity(activity) {
     activities.value.push(activity);
     LocalStorage.setItem("Activities", JSON.stringify(activities.value));
